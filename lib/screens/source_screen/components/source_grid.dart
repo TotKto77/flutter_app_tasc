be885ef70ebe5ad043ca708116aba2d1973b4e15
TotKto77 <favnartt7@gmail.com>
2023-11-16 22:57:51 +0300

    agency details in progress

--- lib/screens/source_screen/components/source_grid.dart
+++ lib/screens/source_screen/components/source_grid.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_tasc/common/app_assets/app_colors.dart';
 import 'package:flutter_app_tasc/logic/models/models_nwes_agansi.dart';
+import 'package:flutter_app_tasc/screens/source_screen/agency_detail_screen.dart';
 
 class AgencyCard extends StatelessWidget {
   final AgencyModel agency;
@@ -9,36 +10,46 @@ class AgencyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: AppClor.background,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 7,
-              offset: const Offset(0, 2),
-              spreadRadius: 1,
-              color: Colors.white.withOpacity(0.5),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => AgencyDetailScreen(agency: agency),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: AppClor.background,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 7,
+                offset: const Offset(0, 2),
+                spreadRadius: 1,
+                color: Colors.white.withOpacity(0.5),
+              ),
+            ],
+            borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(bottom: 10),
+              height: 50,
+              width: 50,
+              alignment: Alignment.center,
+              child: Image.asset(agency.imageUrl),
+            ), // Предполагается, что это путь к изображению
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                agency.name,
+                style: AppStyleText.titleAgencyGridText,
+              ),
             ),
           ],
-          borderRadius: BorderRadius.circular(12)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(bottom: 10),
-            height: 50,
-            width: 50,
-            alignment: Alignment.center,
-            child: Image.asset(agency.imageUrl),
-          ), // Предполагается, что это путь к изображению
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              agency.name,
-              style: AppStyleText.titleAgencyGridText,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
@@ -53,8 +64,8 @@ class AgencyGrid extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 8,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
       ),
       itemCount: AgencyModel.staticAgencyList.length,
       itemBuilder: (context, index) {
