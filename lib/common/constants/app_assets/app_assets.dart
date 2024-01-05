71e733615909381bf099a23fc306886ebab93941
TotKto77 <favnartt7@gmail.com>
2024-01-05 14:51:56 +0300

    5.1.24.fix2

--- lib/common/constants/app_assets/app_assets.dart
+++ lib/common/constants/app_assets/app_assets.dart
@@ -6,10 +6,10 @@ abstract class AppAssets {
 class _Images {
   const _Images();
   final String standardNwes = 'assets/images/bitmap/basic1.png';
-  final String standardLogo = 'assets/images/bitmap/logo.png';
+  final String standardLogo = 'assets/icons/icon.png';
   final String standardHot = 'assets/images/bitmap/hotnwes.png';
   final String stamdardAppLogo = 'assets/images/bitmap/applogo.png';
-  final String stamdardImageNwesList = 'assets/icons/icon.png';
+  final String stamdardImageNwesList = 'assets/images/bitmap/standard.png';
   final String nullDataUrl = 'assets/images/bitmap/urll_null.png';
   final String errorImagi = 'assets/images/bitmap/something_go_wrong.png';
 }
