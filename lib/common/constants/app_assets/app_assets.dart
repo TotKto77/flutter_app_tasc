164a337d753bd0afc1bddd799ac31a789669a479
TotKto77 <favnartt7@gmail.com>
2024-01-05 14:31:26 +0300

    1.5.24.fix

--- lib/common/constants/app_assets/app_assets.dart
+++ lib/common/constants/app_assets/app_assets.dart
@@ -9,7 +9,7 @@ class _Images {
   final String standardLogo = 'assets/images/bitmap/logo.png';
   final String standardHot = 'assets/images/bitmap/hotnwes.png';
   final String stamdardAppLogo = 'assets/images/bitmap/applogo.png';
-  final String stamdardImageNwesList = 'assets/images/bitmap/standard.png';
+  final String stamdardImageNwesList = 'assets/icons/icon.png';
   final String nullDataUrl = 'assets/images/bitmap/urll_null.png';
   final String errorImagi = 'assets/images/bitmap/something_go_wrong.png';
 }
